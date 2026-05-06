eea09da1a33c04c0b1c31b5e4575994d9b23f6a2
DevOps Automation <devops@jclee.me>
2026-05-07 02:17:18 +0900

    chore: remove obsolete workspaces and services
    Remove deleted workspace artifacts:
    - 107-supabase (entire workspace)
    - 108-archon (entire workspace)
    - lxc-100-proxy (removed container)
    - lxc-101-gitlab-runner (replaced by runner)
    - gitlab-runner scripts and checklists
    - grafana-datasources, archon/supabase traefik routes
    - Old deployment scripts (nfs-cache, verify-nfs-cache, etc.)

--- 108-archon/terraform/variables.tf
+++
@@ -1,3 +0,0 @@
-# Config deployment (docker-compose, .env, filebeat) is owned by 100-pve
-# via config-renderer templates with onepassword_secrets variables.
-# No variables needed in this skeleton workspace.
